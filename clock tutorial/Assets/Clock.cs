b71278b1749f4f6e0c1f83f263e32fcc1d71611f
Nick Beard <nb234515@ohio.edu>
2017-02-02 13:20:34 -0500

    Clock Seconds

---
+++ clock tutorial/Assets/Clock.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion euler = Quaternion.Euler (
+			                   new Vector3 (0f, 0f, Time.time * 360f));
+		
+		transform.localRotation = euler;
+	}
+}
